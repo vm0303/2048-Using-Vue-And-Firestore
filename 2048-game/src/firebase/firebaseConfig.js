12e9cf689ee9ae56968a9047603a15294465eb19
Vishal Madhav <57302592+vm0303@users.noreply.github.com>
2023-02-27 15:11:03 -0500

    Reverted my changes of hiding the API key because I'm only add the data locally rather than use a real-time database.

--- 2048-game/src/firebase/firebaseConfig.js
+++ 2048-game/src/firebase/firebaseConfig.js
@@ -1,10 +1,10 @@
 export default {
-    apiKey: process.env.API_KEY,
+    apiKey: 'AIzaSyAmAaHJlj08U9xaX8XAzl1VuvwFIFjJdlA',
     authDomain: 'game-d8449.firebaseapp.com',
     projectId: 'game-d8449',
     storageBucket: 'game-d8449.appspot.com',
-    messagingSenderId: process.env.SENDER_ID,
-    appId: process.env.APP_ID,
-    measurementId: process.env.MEASURE_ID
+    messagingSenderId: '902276822374',
+    appId: '1:902276822374:web:bc4f1a0dd6fb5a892504a7',
+    measurementId: 'G-3V3J6LGYBE'
 }
 
